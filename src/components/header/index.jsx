7cea48ec09ed57950f209e29f88b645011a6863b
nghienthamnien <ptbinh2w@gmail.com>
2023-12-01 11:55:03 +0700

    fix header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Logo from './logo';
 import './index.css';
 import Avatar from './avatar';
 
 export default function Header() {
+    const isAuthenticate = useSelector((state) => state.auths.isAuthenticate);
+    if (isAuthenticate) {
+        return (
+            <div className="header-bar">
+                <Logo />
+                <Avatar />
+            </div>
+        );
+    }
     return (
-        <div className="header-bar">
+        <div className="header-bar" style={{ justifyContent: 'center' }}>
             <Logo />
-            <Avatar />
         </div>
     );
 }
--- src/components/header/userCard.jsx
+++ src/components/header/userCard.jsx
@@ -1,16 +1,39 @@
 import React from 'react';
-import { Card } from 'antd';
-const App = () => (
-    <Card
-        title="User Name"
-        bordered={false}
-        style={{
-            width: 250,
-        }}
-    >
-        <p>Card content</p>
-        <p>Card content</p>
-        <p>Card content</p>
-    </Card>
-);
+import { Card, Button } from 'antd';
+import { useDispatch } from 'react-redux';
+import { updateAuthenticate } from '../../slice/authsSlice';
+const App = () => {
+    const user = JSON.parse(localStorage.getItem('user_info'));
+    const userName = user ? user.name : 'User Name';
+    const dispatch = useDispatch();
+    const handleClick = () => {
+        localStorage.removeItem('auth_token');
+        localStorage.removeItem('user_info');
+        dispatch(updateAuthenticate(false));
+    };
+    return (
+        <Card
+            title={`${userName}`}
+            bordered={false}
+            style={{
+                width: 250,
+            }}
+        >
+            <div>
+                <p>Card content</p>
+            </div>
+            <div>
+                <p>Card content</p>
+            </div>
+            <div>
+                <p>Card content</p>
+            </div>
+            <div>
+                <Button type="primary" size={'large'} onClick={handleClick}>
+                    Log out
+                </Button>
+            </div>
+        </Card>
+    );
+};
 export default App;
