3fe12cd2dcb7900b0650eb1b3b1284c3070afb89
nghienthamnien <ptbinh2w@gmail.com>
2023-12-01 11:54:41 +0700

    add footer

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+    FacebookFilled,
+    YoutubeFilled,
+    InstagramFilled,
+    LinkedinFilled,
+} from '@ant-design/icons';
+import './index.css';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer-icon">
+                <a href="https://facebook.com" type="text">
+                    <FacebookFilled style={{ fontSize: '32px' }} />
+                </a>
+                <a type="text" href="https://youtube.com">
+                    <YoutubeFilled style={{ fontSize: '32px' }} />
+                </a>
+                <a type="text" href="https://instagram.com">
+                    <InstagramFilled style={{ fontSize: '32px' }} />
+                </a>
+                <a type="text" href="https://linked.com">
+                    <LinkedinFilled style={{ fontSize: '32px' }} />
+                </a>
+            </div>
+            <div className="copyright">
+                <p>Công nghệ Web và dịch vụ trực tuyến &copy; Nhóm 3</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
